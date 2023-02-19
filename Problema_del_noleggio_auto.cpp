bc3e5ece7f814b1d793d068cd6824f80265833f6
IsmaeleCalifano <118549602+IsmaeleCalifano@users.noreply.github.com>
2023-02-19 13:06:54 +0100

    Update Problema_del_noleggio_auto.cpp

--- Problema_del_noleggio_auto.cpp
+++ Problema_del_noleggio_auto.cpp
@@ -6,17 +6,27 @@ using namespace std;
 
 const int NUM_NOLEGGIATORI = 5;
 
+int pos;
 bool possiedeChiavi[NUM_NOLEGGIATORI];
+pthread_t noleggiatori[NUM_NOLEGGIATORI];
 
 int schermoMenu();
 int leggiInteroLimitato(int min, int max, string messaggio);
 void inizializzaBooleanArrayFalse(bool booleanArray[NUM_NOLEGGIATORI]);
+void creaThread(pthread_t noleggiatori[NUM_NOLEGGIATORI], int* numeroChiavi);
+void stampaStatoNoleggiatore(int pos, string messaggio);
+void* prendiChiave(void* numeroChiavi);
+void wait(int& s);
+void signal(int& s);
 
 int main() {
     int scelta = schermoMenu();
     if (scelta == 1) {
         int numeroChiavi = NUM_NOLEGGIATORI;
         inizializzaBooleanArrayFalse(possiedeChiavi);
+        while (true) {
+            creaThread(noleggiatori, &numeroChiavi);
+        }
     }
     return 0;
 }
@@ -71,4 +81,50 @@ void inizializzaBooleanArrayFalse(bool booleanArray[NUM_NOLEGGIATORI]) {
     for (int i = 0; i < NUM_NOLEGGIATORI; i++) {
         possiedeChiavi[i] = false;
     }
+}
+
+void creaThread(pthread_t noleggiatori[NUM_NOLEGGIATORI], int* numeroChiavi) {
+    for(pos = 0; pos < NUM_NOLEGGIATORI; pos++){
+        sleep(1);
+        int thread = pthread_create(&noleggiatori[pos], NULL, prendiChiave, (void*)numeroChiavi);
+        pthread_join(noleggiatori[pos], 0);
+    }
+}
+
+void stampaStatoNoleggiatore(int pos, string messaggio) {
+    cout << endl << "Il noleggiatore n." << pos + 1 << " " << messaggio << endl;
+}
+
+void* prendiChiave(void* numeroChiavi) {
+    int n = NUM_NOLEGGIATORI / 2;
+    pthread_join((long unsigned int)noleggiatori[pos], 0);
+    if ((*(int*)numeroChiavi >= n) && (possiedeChiavi[pos] == false)) {
+        wait(*(int*)numeroChiavi);
+        possiedeChiavi[pos] = true;
+        stampaStatoNoleggiatore(pos, "HA PRESO LA CHIAVE");
+        cout << "Numero chiavi: " << *(int*)numeroChiavi << endl;
+        pthread_join(noleggiatori[pos],0);
+        pthread_exit(0);
+    } else if ((*(int*)numeroChiavi < n) && (possiedeChiavi[pos] == true)) {
+        stampaStatoNoleggiatore(pos, "STA GUIDANDO");
+        possiedeChiavi[pos] = false;
+        signal(*(int*)numeroChiavi);
+        cout << "Numero chiavi: " << *(int*)numeroChiavi << endl;
+        pthread_exit(0);
+    } else if ((*(int*)numeroChiavi < n) && (possiedeChiavi[pos] == false)) {
+        stampaStatoNoleggiatore(pos, "STA RIPOSANDO");
+        pthread_exit(0);
+    }
+    pthread_exit(0);
+}
+
+void wait(int& s) {
+    while (s < 0) {
+        pthread_exit(0);
+    }
+    s--;
+}
+
+void signal(int& s) {
+    s++;
 }
